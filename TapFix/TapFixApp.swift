4482b935a08bb36ad6f6181af0569e16a5eb4aea
nacholas <1348513+nacholas@users.noreply.github.com>
2022-12-10 12:25:19 -0500

    Initial Commit

---
+++ TapFix/TapFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapFixApp.swift
+//  TapFix
+//
+//  Created by Nicholas Dehnen on 2022-12-10.
+//
+
+import SwiftUI
+
+@main
+struct TapFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
