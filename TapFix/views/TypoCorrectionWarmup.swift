adbabb78c8058224f6e978cf16228af8c370d474
nacholas <1348513+nacholas@users.noreply.github.com>
2022-12-10 17:39:18 -0500

    Typo correction layout designed

--- TapFix/views/TypoCorrectionWarmup.swift
+++ TapFix/views/TypoCorrectionWarmup.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct TypoCorrectionWarmup: View {
+
+    @State var text: String = "i love my fiye cats";
+    @State var sentenceStart: String = "i love my";
+    @State var sentencePieceFaulty: String = "fiye";
+    @State var sentencePieceCorrect: String = "five";
+    @State var sentenceEnd: String = "cats";
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("Correct the following sentence:")
+                .font(.headline)
+                .padding(.bottom, 3.0)
+            HStack(alignment: .top) {
+                Text(sentenceStart)
+                VStack {
+                    Text(sentencePieceFaulty)
+                        .foregroundColor(Color.red)
+                        .underline()
+                    Image(systemName: "arrow.down")
+                }
+                Text(sentenceEnd)
+            }
+            HStack(alignment: .top) {
+                Text(sentenceStart)
+                Text(sentencePieceCorrect)
+                    .foregroundColor(Color.green)
+                Text(sentenceEnd)
+            }
+            TextField("Please wait..", text: $text)
+                .keyboardType(.alphabet)
+                .disableAutocorrection(true)
+                .textInputAutocapitalization(.never)
+                .textFieldStyle(.roundedBorder)
+                .multilineTextAlignment(.center)
+                .padding()
+            Spacer()
+        }
+        .padding()
+        .ignoresSafeArea(.keyboard, edges: .bottom)
     }
 }
 
